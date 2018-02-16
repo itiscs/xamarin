b3f19db200fa90f2e074ff317fc56e2307c50b46
moudroff <moudroff>
2018-02-16 14:38:38 +0300

    XamApp

---
+++ XamApp/XamApp/XamApp/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamApp
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+		}
+
+        private void btn1_Clicked(object sender, EventArgs e)
+        {
+            lbl1.Text = $"Привет, {txtName.Text}!";
+            
+        }
+
+        private void btnNext_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new SecondPage());
+
+        }
+
+        private void btnThird_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new GridPage());
+
+
+        }
+    }
+}
---
+++ XamApp/XamApp/XamApp/SecondPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SecondPage : ContentPage
+	{
+		public SecondPage ()
+		{
+			InitializeComponent ();
+
+            TapGestureRecognizer tapGesture = new TapGestureRecognizer();
+
+            tapGesture.Tapped += TapGesture_Tapped;
+
+            lbl1.GestureRecognizers.Add(tapGesture);
+
+            //imgPlus.Source = Device.OnPlatform(
+            //    Android: ImageSource.FromResource("plus.png"),
+            //    WinPhone: ImageSource.FromResource("plus.png"));
+
+
+        }
+
+        private void TapGesture_Tapped(object sender, EventArgs e)
+        {
+            BoxView box = new BoxView()
+            {                
+                WidthRequest = 100,
+                HeightRequest = 100,
+                Color = Color.Green
+            };
+            stk1.Children.Add(box);
+        }
+    }
+}
